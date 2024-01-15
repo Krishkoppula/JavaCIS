129c556e61fa0c595cb1cc1d0b123bec484ff17e
Krishkoppula <saikrishna.usk10@gmail.com>
2024-01-15 11:55:44 -0600

    Java Training

---
+++ src/Biodata.java
@@ -0,0 +1,19 @@
+import java.lang.String;
+
+public class Biodata {
+
+    public static void main(String[] args){
+        String[] names={"Sai","Krishna","Reddy","Koppula","Soumya","Red","Sairam","Thokala","Apple","Krish"};
+       for(int i=0;i< names.length;i++)
+        {
+            System.out.println(names[i]);
+        }
+        System.out.println("---------------------------------------");
+        System.out.println(names[0]);
+        System.out.println(names[9]);
+
+
+
+    }
+
+}
